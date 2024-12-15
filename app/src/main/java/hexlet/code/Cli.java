2dd3b159462985aeab2d6a6cb6cafe22559ccf00
xcenia <xcenia94@gmail.com>
2024-12-15 15:08:44 +0700

    add object cli.getname() in Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,13 +2,16 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void welcome() {
-        Scanner scanner = new Scanner(System.in);
+    private final String name;
 
+    public Cli(Scanner scanner) {
         System.out.println("May I have your name?");
-        String name = scanner.next();
+        this.name = scanner.next();
+    }
+    public String getName() {
+        return name;
+    }
+    public void welcome() {
         System.out.println("Hello, " + name + "!");
-
-        scanner.close();
     }
 }
