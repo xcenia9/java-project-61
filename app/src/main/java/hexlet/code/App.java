1d5e534568d3305e303ed21f2d1df5c3c551da97
xcenia <xcenia94@gmail.com>
2024-12-15 15:07:36 +0700

    feat: add class new method and feat style code in App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,28 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.welcome();
+        Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Exit");
+            String choice = scanner.next();
+            System.out.println("Your choice: " + choice);
+
+            switch (choice) {
+                case "1":
+                    System.out.println("Welcome to the Brain Games!");
+                    Cli cli = new Cli(scanner);
+                    cli.welcome();
+                    break;
+                case "2":
+                    Even.gameEven(scanner);
+                    break;
+                default:
+                    break;
+            }
+
+        scanner.close();
     }
 }
