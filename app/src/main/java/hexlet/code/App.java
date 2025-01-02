2641515b904fc8c50d45ac6b078c60998ce00bd4
xcenia <xcenia94@gmail.com>
2025-01-02 15:29:09 +0700

    update app.java, add new game Calc and fix method Cli.welcome()

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,23 +1,33 @@
 package hexlet.code;
 
+import hexlet.code.games.*;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-            System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Exit");
+            System.out.println("Please enter the game number and press Enter.");
+            System.out.println("1 - Greet");
+            System.out.println("2 - Even");
+            System.out.println("3 - Calc");
+            System.out.println("0 - Exit");
             String choice = scanner.next();
             System.out.println("Your choice: " + choice);
 
             switch (choice) {
                 case "1":
                     System.out.println("Welcome to the Brain Games!");
-                    Cli cli = new Cli(scanner);
-                    cli.welcome();
+                    Cli.welcome();
                     break;
                 case "2":
-                    Even.gameEven(scanner);
+                    Even.gameEven();
+                    break;
+                case "3":
+                    Calc.gameCalc();
+                    break;
+                case "0":
                     break;
                 default:
                     break;
