542f2bbea17fbe0643fc13e37cebff860fad8351
xcenia <xcenia94@gmail.com>
2025-01-02 15:29:29 +0700

    add new class Calc.java

---
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -0,0 +1,36 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+public class Calc {
+    private static String[] getQuestionAndAnswer(){
+        String[] operators = {"+", "-", "*"};
+        int a = (int) (Math.random() * 100);
+        int b = (int) (Math.random() * 100);
+        int randomOperator = (int) (Math.random() * operators.length);
+        String operator = operators[randomOperator];
+        String question = (a + " " + operator + " " + b);
+        int result = 0;
+        switch (operator) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result  = a - b;
+                break;
+            case "*":
+                result  = a * b;
+                break;
+        }
+        String correctAnswer = "" + result;
+        return new String[]{question, correctAnswer};
+    }
+    public static void gameCalc(){
+        String exercise = "What is the result of the expression?";
+        String[][] questionAndAnswer = new String[Engine.countOfRounds][2];
+        for (int i = 0; i < Engine.countOfRounds; i++) {
+            questionAndAnswer[i] = getQuestionAndAnswer();
+        }
+        Engine.gameEngine(exercise, questionAndAnswer);
+    }
+}
