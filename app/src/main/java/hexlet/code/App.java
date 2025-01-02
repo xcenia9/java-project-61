5d61bba81e9864614bfe114345972a451a46d5ba
xcenia <xcenia94@gmail.com>
2025-01-02 16:31:44 +0700

    update App.java, new game GCD

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -12,6 +12,7 @@ public class App {
             System.out.println("1 - Greet");
             System.out.println("2 - Even");
             System.out.println("3 - Calc");
+            System.out.println("4 - GCD");
             System.out.println("0 - Exit");
             String choice = scanner.next();
             System.out.println("Your choice: " + choice);
@@ -26,6 +27,9 @@ public class App {
                     break;
                 case "3":
                     Calc.gameCalc();
+                    break;
+                case "4":
+                    Gcd.gameGcd();
                     break;
                 case "0":
                     break;
