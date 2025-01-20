5c3fecf3a13ac885dcdd8c8849443617ec7b64e4
xcenia <xcenia94@gmail.com>
2025-01-20 23:09:27 +0700

    add testing file AppTest.java

---
+++ app/src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testExample() {
+        assertEquals(2, 1 + 1);
+    }
+}
